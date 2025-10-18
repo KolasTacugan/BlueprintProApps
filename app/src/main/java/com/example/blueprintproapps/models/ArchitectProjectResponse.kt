fba8958c0944b9667ec0046a0b5a5d2f7dc497eb
Gene Harold <geneisme20@gmail.com>
2025-10-19 03:19:41 +0800

    CAN NOW UPLOAD TO PROJECT

---
+++ app/src/main/java/com/example/blueprintproapps/models/ArchitectProjectResponse.kt
@@ -0,0 +1,11 @@
+package com.example.blueprintproapps.models
+
+data class ArchitectProjectResponse(
+    val project_Id: String,
+    val project_Title: String,
+    val project_Budget: String?,
+    val project_Status: String,
+    val blueprint_Id: Int,
+    val blueprintImage: String?,
+    val clientName: String
+)
